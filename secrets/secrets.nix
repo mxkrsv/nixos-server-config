5faf982dbaea5e0417412196bc1f2e8c148c3ddf
Maxim Karasev <mxkrsv@disroot.org>
2024-03-15 18:59:07 +0300

    secrets: update recipients and replace porkbun keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,11 @@
 let
+  mxkrsv-sayaka = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMkupsms0fUp4SflgKwVBfKRXi8eY51cgbex4aXerC5B mxkrsv@sayaka";
+  users = [ mxkrsv-sayaka ];
+
   kyoko = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINTaDclKz6lF3VYdHX+ty/hEeF3CPrpQdY3bRJqyUI6b";
   systems = [ kyoko ];
 in
 {
-  "porkbun_api_key.age".publicKeys = systems;
-  "porkbun_secret_api_key.age".publicKeys = systems;
+  "porkbun_api_key.age".publicKeys = systems ++ users;
+  "porkbun_secret_api_key.age".publicKeys = systems ++ users;
 }
