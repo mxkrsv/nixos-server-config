7e482dceb57175aa5d990207f233d7940b38ea00
Maxim Karasev <mxkrsv@disroot.org>
2024-03-15 19:03:47 +0300

    host: import shadowsocks password from agenix

--- host/common/secrets.nix
+++ host/common/secrets.nix
@@ -2,5 +2,7 @@
   age.secrets = {
     porkbun_api_key.file = ../../secrets/porkbun_api_key.age;
     porkbun_secret_api_key.file = ../../secrets/porkbun_secret_api_key.age;
+
+    shadowsocks_password.file = ../../secrets/shadowsocks_password.age;
   };
 }
