1b296b849744a2ca30ace42e1b3f3b5511ec3e82
Maxim Karasev <mxkrsv@disroot.org>
2024-03-19 14:01:06 +0300

    host: import sing-box uuid from agenix

--- host/common/secrets.nix
+++ host/common/secrets.nix
@@ -2,5 +2,7 @@
   age.secrets = {
     porkbun_api_key.file = ../../secrets/porkbun_api_key.age;
     porkbun_secret_api_key.file = ../../secrets/porkbun_secret_api_key.age;
+
+    singbox_uuid_mxkrsv.file = ../../secrets/singbox_uuid_mxkrsv.age;
   };
 }
