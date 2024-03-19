afb445392bfceee2380fd4a2ea78258685fdfee6
Maxim Karasev <mxkrsv@disroot.org>
2024-03-19 14:01:06 +0300

    host: import xray uuid from agenix

--- host/common/secrets.nix
+++ host/common/secrets.nix
@@ -2,5 +2,7 @@
   age.secrets = {
     porkbun_api_key.file = ../../secrets/porkbun_api_key.age;
     porkbun_secret_api_key.file = ../../secrets/porkbun_secret_api_key.age;
+
+    xray_uuid_mxkrsv.file = ../../secrets/xray_uuid_mxkrsv;
   };
 }
