143454e2d63e099289fc26eb91b878f00d8f3cd4
Maxim Karasev <mxkrsv@disroot.org>
2024-03-19 13:56:51 +0300

    secrets: add xray uuid

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,8 +4,11 @@ let
 
   kyoko = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINTaDclKz6lF3VYdHX+ty/hEeF3CPrpQdY3bRJqyUI6b";
   systems = [ kyoko ];
+  xraySystems = [ kyoko ];
 in
 {
   "porkbun_api_key.age".publicKeys = systems ++ users;
   "porkbun_secret_api_key.age".publicKeys = systems ++ users;
+
+  "xray_uuid_mxkrsv".publicKeys = xraySystems ++ users;
 }
